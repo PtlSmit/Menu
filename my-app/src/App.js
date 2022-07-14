6308156ef6984d93f2bd872b1f24404e09026adb
Smit <ptlsmit.b@gmail.com>
2022-07-14 11:15:03 -0700

    Dynamic category list created

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,27 +1,31 @@
-import React, { useState } from 'react'
-import Categories from './Categories'
-import Menu from './Menu'
-import items from './data'
+import React, { useState } from "react";
+import Categories from "./Categories";
+import Menu from "./Menu";
+import items from "./data";
+
+const allCategories = ['all' ,...new Set(items.map((item) => item.category))];
+console.log(allCategories)
+
 const App = () => {
-  const [menuItems,setMenuItems]=useState(items)
-  const [categories, setCategories] = useState([]);
+  const [menuItems, setMenuItems] = useState(items);
+  const [categories, setCategories] = useState(allCategories);
 
   const filterItems = (category) => {
-     const newItems = items.filter((item) => item.category === category)
-     setMenuItems(newItems)
-  }
+    const newItems = items.filter((item) => item.category === category);
+    setMenuItems(newItems);
+  };
   return (
-   <main>
-    <section className='menu section'>
-      <div className='title'>
-        <h2>Our Menu</h2>
-        <div className='underline'></div>
-      </div>
-      <Categories filterItems={filterItems}/>
-      <Menu items={menuItems}/>
-    </section>
-   </main>
-  )
-}
+    <main>
+      <section className="menu section">
+        <div className="title">
+          <h2>Our Menu</h2>
+          <div className="underline"></div>
+        </div>
+        <Categories filterItems={filterItems} categories={categories} />
+        <Menu items={menuItems} />
+      </section>
+    </main>
+  );
+};
 
-export default App
+export default App;
